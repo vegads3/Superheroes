08b41730b91c97f75499576c1861525848064579
vegads3 <tristannbautista02@gmail.com>
2025-11-11 17:51:50 -0600

    recien actualizado

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+// Archivo: settings.gradle.kts
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Superheroes"
+include(":app")
